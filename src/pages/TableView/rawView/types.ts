877feecf3f0de1a0453538dcb78548dd81863ebd
EmGira <emanuelegirardello1@gmail.com>
2025-09-12 10:49:58 +0200

    added license

--- src/pages/TableView/rawView/types.ts
+++ src/pages/TableView/rawView/types.ts
@@ -1,3 +1,12 @@
+    /*
+    <!--
+SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+    */
+    
     export type JsonPrimitive = string | number | boolean | null;
     export type JsonValue = JsonPrimitive | JsonObject | JsonArray;
     export interface JsonObject { [key: string]: JsonValue }
