6719a4eca1ad7b86478ef7d12e1611611f120419
EmGira <emanuelegirardello1@gmail.com>
2025-09-12 10:55:44 +0200

    eslit error fix

--- src/pages/TableView/rawView/types.ts
+++ src/pages/TableView/rawView/types.ts
@@ -9,5 +9,5 @@ SPDX-License-Identifier: AGPL-3.0-or-later
     
     export type JsonPrimitive = string | number | boolean | null;
     export type JsonValue = JsonPrimitive | JsonObject | JsonArray;
-    export interface JsonObject { [key: string]: JsonValue }
-    export interface JsonArray extends Array<JsonValue> {}
+    export type JsonObject = { [key: string]: JsonValue };
+    export type JsonArray = JsonValue[];
