4ed2d217086bdf5db730486ea1d7767f2d358eb0
EmGira <emanuelegirardello1@gmail.com>
2025-09-12 14:41:40 +0200

    removed Image Classifier feature

--- src/components/Image/showImageFullView.vue
+++ src/components/Image/showImageFullView.vue
@@ -31,13 +31,12 @@ SPDX-License-Identifier: AGPL-3.0-or-later
             &times;
         </button>
 
-        <ImageClassifier  :ImageUrl="props.image.ImageUrl"></ImageClassifier>
+
     </div>
 </template>
 
 
 <script setup lang="ts">
-    import ImageClassifier from '@/ImageRecognition/ImageClassifier.vue';
 
     const props = defineProps<{
         image: {
